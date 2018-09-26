2720c5d8a107b6671bc1769367075dc2436e7c5b
mohammad <khezri@outlook.com>
2018-09-25 20:21:21 -0500

    project page finished

--- projects.js
+++ projects.js
@@ -31,4 +31,30 @@ let projects = [
 ];
 
 
-console.log(projects);
+// funtion for Print to DOM
+const printToDom = (stringToPrint, divId) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = stringToPrint;
+};
+
+// function to print projects on webpage
+const createProjectCards = () => {
+    let newProject = '';
+    for (let i = 0; i < projects.length; i++) {
+      newProject += `<div class="cardContainer">`;
+      newProject +=`<h5 class="project_header">Projects</h5>`;
+
+      newProject += `<p>${projects[i].title}</p>`;
+      newProject += `<img src="${projects[i].screenshot}"></img>`;
+      newProject += `<p>${projects[i].description}</p>`;
+      newProject += `<p>${projects[i].technologiesUsed}</p>`;
+      newProject += `<p>${projects[i].available}</p>`;
+      newProject += `<a href="https://${projects[i].url}" target="_blank">View Project</a>`;
+      newProject += `<p><a href="https://${projects[i].githubUrl}" target="_blank">Git Hub</a><p>`;
+      newProject += `</div>`;
+        if (projects[i].available === true){
+        printToDom(newProject, 'projectsPage');
+    };
+    };
+};
+createProjectCards();
