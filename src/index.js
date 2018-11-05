8534caf61b2f8aefdeee3a2554ecfa6f90a81677
mohammad <khezri@outlook.com>
2018-11-04 20:54:42 -0600

    made few changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-import $ from 'jquery';
+// import $ from 'jquery';
 import 'bootstrap';
 import './index.scss';
 
-$('#click-button').on('click', () => {
-  $('#important-text').append('<p>Hello World!</p>');
-});
 
 
 // // funtion for Print to DOM
