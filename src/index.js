9adbbebfafc634d34bde9f3dd5dd9697a807ac3f
mohammad <khezri@outlook.com>
2018-11-06 19:44:37 -0600

    printed projects

--- src/index.js
+++ src/index.js
@@ -20,13 +20,13 @@ const loadProjects = (projects) => {
   projects.forEach((project) => {
     newString += `
         <div class="mr-2">
-        <p>${projects.title}</p>
-        <img src="${projects.screenshot}"></img>
-        <p>${projects.description}</p>
-        <p>${projects.technologiesUsed}</p>
-        <p>${projects.available}</p>
-        <a href="https://${projects.url}" target="_blank">View Project</a>
-        <p><a href="https://${projects.githubUrl}" target="_blank">Git Hub</a><p>
+        <p>${project.title}</p>
+        <img src="${project.screenshot}"></img>
+        <p>${project.description}</p>
+        <p>${project.technologiesUsed}</p>
+        <p>${project.available}</p>
+        <a href="https://${project.url}" target="_blank">View Project</a>
+        <p><a href="https://${project.githubUrl}" target="_blank">Git Hub</a><p>
         </div>`;
     if (project.available === true) {
       $('#my-projects').html(newString);
