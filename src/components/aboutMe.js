aa0a10d2c7866f3bcf8df4ef85f086d4e4fb8a8a
mohammad <khezri@outlook.com>
2018-12-17 20:52:05 -0600

    changes the bio paragraph

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -11,24 +11,9 @@ const AboutMe = () => {
       | Download Resume</button>
   </div>
   <div data-aos="fade-up" class="paragraph-holder flex-item text-secondary m-5">
+  <h1 class="display-4">Mohammad Khezri</h1>
     <p>I am a graphic designer, art director, and photographer for over the past 15 years. I have gained great
-      experiences in the fields of printing and layout design. After finishing high school, I soon began my first
-      career in a printing shop as a junior graphic designer in 1999. I was in charge of creating different type of
-      artworks for different clients.</p>
-    <p> In 2004 I was employed by a large scale company in the region as a graphic designer. I acted as a graphic
-      designer and photographer for a few years then soon I promoted to a senior graphic designer and art director.
-      I
-      was responsible for supervise other graphic designers to make sure every project meets the principles and
-      deadlines. The graphic design division as part of marketing department was responsible to support sales and
-      marketing needs. Our major projects were creating variety of brochures, catalogs, calendars, price list
-      booklets,
-      advertising for magazines, sign boards for agents, support websites, commercial and product photography.</p>
-    <p>Since I studied computer in high school and I had some experince in some languages such as GW-Basic, C and
-      Pascal, I decided to learn web programming technologies so I enrolled at Nashville software school to become
-      a
-      full stack web developer because I belive it will open a lot of doors for me. Moreover, I love creating
-      pretty
-      things as a graphic designer and I think there are a lot of oppurtunities in web and application design. </P>
+      experiences in the fields of printing and layout design. After immigrating to the US in 2017 I decided to make a change to my career to web development because I believe web development is a combiniation of design and technology...</p>
   </div>
     `;
   $('#about-me').html(domString);
